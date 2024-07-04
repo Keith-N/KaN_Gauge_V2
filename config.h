04204f2220f1e297614b3927daab546e2557bbf2
Keith <Keith-N@users.noreply.github.com>
2024-07-04 00:21:55 -0400

    version updates

--- config.h
+++ config.h
@@ -12,9 +12,9 @@
 #pragma once
 
 int bootConfig;
-const String build = "0.38";
+const String build = "0.39";
 const int nvs = 4;
-const String buildType = "";
+const String buildType = "Testing";
 
 // revision of PCB 1.2 = 102
 int pcbRev = 102;
